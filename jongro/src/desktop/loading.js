68fb9e40b4fd89bdf13c0c15f78a729a46d52ce5
kanghea <kanghea26@gmail.com>
2022-11-23 17:19:36 +0900

    RE:EF

--- jongro/src/desktop/loading.js
+++ jongro/src/desktop/loading.js
@@ -1,8 +1,9 @@
+
 function Loading(){
     setTimeout(() => {window.location.href='/main';}, 3000);
     return(
     <div className="w-screen h-screen">
-        <img src="public/img/loading.png" className="w-full h-full"/>  
+        <img src="./loading.png" className="w-full h-full"/>  
     </div>)
 }
 
