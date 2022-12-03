f772951393fa07725f369986585baa49cd6173ac
kanghea <kanghea26@gmail.com>
2022-12-03 20:02:53 +0900

    RE:FE

--- jongro/src/mobile/student/Navbar.js
+++ jongro/src/mobile/student/Navbar.js
@@ -1,13 +1,28 @@
-function Navbar(){
-    
-    return(
+function Navbar() {
+
+    return (
         <div alt="navbar" className="fixed flex w-full p-3 justify-between bottom-0 border-t-2">
-        <div>홈</div>
-        <div>통계</div>
-        <div>QnA</div>
-        <div>랭킹</div>
-        <div>나</div>
-    </div>
+            <a className="flex flex-col justify-center items-center text-sm">
+                <img src="/img/homeicon.png" className="w-5" />
+                홈
+            </a>
+            <a className="flex flex-col justify-center items-center text-sm">
+                <img src="/img/통계.png" className="w-5" />
+                통계
+            </a>
+            <a className="flex flex-col justify-center items-center text-sm">
+                <img src="/img/QnA.png" className="w-5" />
+                QnA
+            </a>
+            <a className="flex flex-col justify-center items-center text-sm">
+                    <img src="/img/랭킹.png" className="w-5" />
+                    랭킹
+            </a>
+            <a className="flex flex-col justify-center items-center text-sm">
+                    <img src="/img/나.png" className="w-5" />
+                    나
+            </a>
+        </div>
     )
 }
 
